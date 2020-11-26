5afa9a5232898efaf38ac7b6d6515de50a1d5bca
Jayme Rutkoski <jaymerut@gmail.com>
2020-11-26 12:11:44 -0700

    Added MapView to ViewController 	-- Added MapKit 	-- Added CLLocation

--- FoodTruckCompany/ViewControllers/FindFoodTrucksViewController.swift
+++ FoodTruckCompany/ViewControllers/FindFoodTrucksViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 import SnapKit
+import MapKit
+import CoreLocation
 
 
-class FindFoodTrucksViewController: UIViewController {
+class FindFoodTrucksViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     
     // MARK: - Variables
-    
-    
+    let distanceSpan: Double = 50
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView.init(frame: .zero)
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsUserLocation = true
+        
+        return mapView
+    }()
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = 5
+        
+        return manager
+    }()
     
     // MARK: - Initialization
     private func customInitFindFoodTrucksViewController() {
@@ -44,6 +64,9 @@ class FindFoodTrucksViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
+        
+        self.getUserCoordinates()
+        
         // Setup
         setupFindFoodTrucksViewController()
     }
@@ -61,12 +84,41 @@ class FindFoodTrucksViewController: UIViewController {
     // MARK: - Private API
     private func setupFindFoodTrucksViewController() {
         
-        // Self
-        
+        self.view.addSubview(self.mapView)
+        self.mapView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+        }
         
     }
+    private func getUserCoordinates() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+    }
     
+    // MARK: Delegate Methods
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        let region = MKCoordinateRegion.init(center: newLocation.coordinate, latitudinalMeters: self.distanceSpan, longitudinalMeters: self.distanceSpan)
+        self.mapView.setRegion(region, animated: true)
+        self.mapView.showsUserLocation = true
+        }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        
+        let currentLocation:CLLocation = locations.last ?? CLLocation.init()
+        
+        let mapCamera = MKMapCamera(lookingAtCenter: locValue, fromEyeCoordinate: locValue, eyeAltitude: 10000)
+        mapView.setCamera(mapCamera, animated: true)
+        
+        manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedAlways || status == CLAuthorizationStatus.authorizedWhenInUse {
+            manager.startUpdatingLocation()
+        }
+    }
     
     // MARK: - Public API
     
