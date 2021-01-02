617e1cd8753b097e3dafa69363046be724150f53
Jayme Rutkoski <jaymerut@gmail.com>
2021-01-02 12:08:28 -0700

    Update ForgotPassword UI

--- TruckYums/ViewControllers/ForgotPasswordViewController.swift
+++ TruckYums/ViewControllers/ForgotPasswordViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 
-class ForgotPasswordViewController: UIViewController {
+class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
     
     
     // MARK: - Variables
+    private let colorInProgress: UIColor = UIColor.init(hex: "0x3C9ADC")!
+    private let colorRequired: UIColor = UIColor.init(hex: "0xDC3C4A")!
+    private let colorNotRequired: UIColor = UIColor.init(hex: "0xE8ECF0")!
+    private let colorCompleted: UIColor = UIColor.init(hex: "0x3CDC7E")!
+    
     private lazy var contentView: UIView = {
         let view = UIView(frame: .zero)
         view.backgroundColor = UIColor.white
@@ -39,6 +44,34 @@ class ForgotPasswordViewController: UIViewController {
         
         return label
     }()
+    private lazy var labelEmail: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.init(name: "Teko-Regular", size: 18.0)
+        label.text = "Enter Email of Account: *"
+        
+        return label
+    }()
+    private lazy var labelName: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.init(name: "Teko-Regular", size: 18.0)
+        label.text = "Enter Name of Account: *"
+        
+        return label
+    }()
+    private lazy var labelPassword: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.init(name: "Teko-Regular", size: 18.0)
+        label.text = "Enter New Password: *"
+        
+        return label
+    }()
+    private lazy var labelPasswordRepeat: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.init(name: "Teko-Regular", size: 18.0)
+        label.text = "Repeat New Password: *"
+        
+        return label
+    }()
     
     private lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout.init())
@@ -47,6 +80,90 @@ class ForgotPasswordViewController: UIViewController {
         return collectionView
     }()
     
+    private lazy var textFieldEmail: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.delegate = self
+        textField.font = UIFont(name: "Teko-Regular", size: 16.0)
+        textField.placeholder = "Enter Email"
+        textField.layer.borderColor = UIColor.init(hex: "0xE8ECF0")?.cgColor
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 6.0
+        textField.leftView = UIView.init(frame: CGRect.init(x: 0.0, y: 0.0, width: 10.0, height: 1.0))
+        textField.leftViewMode = .always
+        textField.returnKeyType = .next
+        textField.layer.borderColor = self.colorRequired.cgColor
+        
+        return textField
+    }()
+    private lazy var textFieldName: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.delegate = self
+        textField.font = UIFont(name: "Teko-Regular", size: 16.0)
+        textField.placeholder = "Enter Name"
+        textField.layer.borderColor = UIColor.init(hex: "0xE8ECF0")?.cgColor
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 6.0
+        textField.leftView = UIView.init(frame: CGRect.init(x: 0.0, y: 0.0, width: 10.0, height: 1.0))
+        textField.leftViewMode = .always
+        textField.returnKeyType = .next
+        textField.layer.borderColor = self.colorRequired.cgColor
+        
+        return textField
+    }()
+    private lazy var textFieldPassword: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.delegate = self
+        textField.font = UIFont(name: "Teko-Regular", size: 16.0)
+        textField.placeholder = "Enter Password"
+        textField.layer.borderColor = UIColor.init(hex: "0xE8ECF0")?.cgColor
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 6.0
+        textField.leftView = UIView.init(frame: CGRect.init(x: 0.0, y: 0.0, width: 10.0, height: 1.0))
+        textField.leftViewMode = .always
+        textField.returnKeyType = .next
+        textField.isSecureTextEntry = true
+        textField.layer.borderColor = self.colorRequired.cgColor
+        
+        return textField
+    }()
+    private lazy var textFieldPasswordRepeat: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.delegate = self
+        textField.font = UIFont(name: "Teko-Regular", size: 16.0)
+        textField.placeholder = "Enter Password Again"
+        textField.layer.borderColor = UIColor.init(hex: "0xE8ECF0")?.cgColor
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 6.0
+        textField.leftView = UIView.init(frame: CGRect.init(x: 0.0, y: 0.0, width: 10.0, height: 1.0))
+        textField.leftViewMode = .always
+        textField.returnKeyType = .done
+        textField.isSecureTextEntry = true
+        textField.layer.borderColor = self.colorRequired.cgColor
+        
+        return textField
+    }()
+    
+    private lazy var buttonApply: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Change Password", for: .normal)
+        button.setTitleColor(.init(hex: 0x455409), for: .normal)
+        button.titleLabel?.font = UIFont.init(name: "Teko-Regular", size: 18.0)
+        button.addTarget(self, action: #selector(buttonApply_TouchUpInside), for: .touchUpInside)
+        button.backgroundColor = UIColor.init(hex: 0xCDDC91)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 20.0
+        button.layer.maskedCorners = [.layerMinXMaxYCorner]
+        
+        return button
+    }()
     private lazy var buttonClose: UIButton = {
         let button = UIButton(type: .custom)
         button.setTitle("Close", for: .normal)
@@ -56,7 +173,7 @@ class ForgotPasswordViewController: UIViewController {
         button.backgroundColor = UIColor.init(hex: 0xCCCCCC)
         button.clipsToBounds = true
         button.layer.cornerRadius = 20.0
-        button.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        button.layer.maskedCorners = [.layerMaxXMaxYCorner]
         return button
     }()
     
@@ -105,11 +222,17 @@ class ForgotPasswordViewController: UIViewController {
     // MARK: - Private API
     private func setupForgotPasswordViewController() {
         
+        // Tap Gesture
+        let gestureTap: UITapGestureRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(gestureTap_Tap))
+        gestureTap.cancelsTouchesInView = false
+        
+        self.view.addGestureRecognizer(gestureTap)
+        
         // Content View
         self.view.addSubview(self.contentView)
         self.contentView.snp.makeConstraints { (make) in
             make.centerX.equalTo(self.view.snp.centerX)
-            make.centerY.equalTo(self.view.snp.centerY)
+            make.top.equalTo(self.view.snp.top).offset(100)
             make.left.equalTo(self.view.snp.left).offset(50)
             make.right.equalTo(self.view.snp.right).inset(50)
             make.height.equalTo(0).priority(250)
@@ -132,11 +255,20 @@ class ForgotPasswordViewController: UIViewController {
             make.bottom.equalTo(self.viewHeader.snp.bottom).offset(-5)
         }
         
+        // Apply Button
+        self.contentView.addSubview(self.buttonApply)
+        self.buttonApply.snp.makeConstraints { (make) in
+            make.bottom.equalTo(self.contentView.snp.bottom)
+            make.left.equalTo(self.contentView.snp.left)
+            make.right.equalTo(self.contentView.snp.centerX)
+            make.height.equalTo(50)
+        }
+        
         // Close Button
         self.contentView.addSubview(self.buttonClose)
         self.buttonClose.snp.makeConstraints { (make) in
             make.bottom.equalTo(self.contentView.snp.bottom)
-            make.left.equalTo(self.contentView.snp.left)
+            make.left.equalTo(self.contentView.snp.centerX)
             make.right.equalTo(self.contentView.snp.right)
             make.height.equalTo(50)
         }
@@ -147,20 +279,152 @@ class ForgotPasswordViewController: UIViewController {
             make.top.equalTo(self.viewHeader.snp.bottom).offset(20)
             make.left.equalTo(self.contentView.snp.left).offset(10)
             make.right.equalTo(self.contentView.snp.right).offset(-10)
-            make.bottom.equalTo(self.buttonClose.snp.top)
-            make.height.equalTo(140)
+            make.bottom.equalTo(self.buttonClose.snp.top).offset(-20)
+            make.height.equalTo(150)
         }
         
+        // Email
+        self.collectionView.addSubview(self.labelEmail)
+        self.labelEmail.snp.makeConstraints { (make) in
+            make.top.equalTo(self.collectionView.snp.top)
+            make.left.equalTo(self.collectionView.snp.left)
+            make.right.equalTo(self.contentView.snp.centerX).offset(-5)
+            
+        }
+        self.collectionView.addSubview(self.textFieldEmail)
+        self.textFieldEmail.snp.makeConstraints { (make) in
+            make.top.equalTo(self.labelEmail.snp.bottom).offset(5)
+            make.left.equalTo(self.collectionView.snp.left)
+            make.right.equalTo(self.contentView.snp.centerX).offset(-5)
+            make.height.equalTo(40)
+        }
         
+        // Name
+        self.collectionView.addSubview(self.labelName)
+        self.labelName.snp.makeConstraints { (make) in
+            make.top.equalTo(self.textFieldEmail.snp.bottom).offset(5)
+            make.left.equalTo(self.collectionView.snp.left)
+            make.right.equalTo(self.contentView.snp.centerX).offset(-5)
+        }
+        self.collectionView.addSubview(self.textFieldName)
+        self.textFieldName.snp.makeConstraints { (make) in
+            make.top.equalTo(self.labelName.snp.bottom).offset(5)
+            make.left.equalTo(self.collectionView.snp.left)
+            make.right.equalTo(self.contentView.snp.centerX).offset(-5)
+            make.height.equalTo(40)
+        }
+        
+        // Password
+        self.collectionView.addSubview(self.labelPassword)
+        self.labelPassword.snp.makeConstraints { (make) in
+            make.top.equalTo(self.collectionView.snp.top)
+            make.left.equalTo(self.contentView.snp.centerX).offset(5)
+            make.right.equalTo(self.contentView.snp.right).offset(-10)
+        }
+        self.collectionView.addSubview(self.textFieldPassword)
+        self.textFieldPassword.snp.makeConstraints { (make) in
+            make.top.equalTo(self.labelPassword.snp.bottom).offset(5)
+            make.left.equalTo(self.contentView.snp.centerX).offset(5)
+            make.right.equalTo(self.contentView.snp.right).offset(-10)
+            make.height.equalTo(40)
+        }
+        
+        // Password Repeat
+        self.collectionView.addSubview(self.labelPasswordRepeat)
+        self.labelPasswordRepeat.snp.makeConstraints { (make) in
+            make.top.equalTo(self.textFieldPassword.snp.bottom).offset(5)
+            make.left.equalTo(self.contentView.snp.centerX).offset(5)
+            make.right.equalTo(self.contentView.snp.right).offset(-10)
+        }
+        self.collectionView.addSubview(self.textFieldPasswordRepeat)
+        self.textFieldPasswordRepeat.snp.makeConstraints { (make) in
+            make.top.equalTo(self.labelPasswordRepeat.snp.bottom).offset(5)
+            make.left.equalTo(self.contentView.snp.centerX).offset(5)
+            make.right.equalTo(self.contentView.snp.right).offset(-10)
+            make.height.equalTo(40)
+        }
+        
+    }
+    
+    private func changePassword() -> Bool {
+        if verifyForm() {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private func verifyForm() -> Bool {
+        if self.textFieldEmail.text?.count == 0 ||
+            self.textFieldName.text?.count == 0 ||
+            self.textFieldPassword.text?.count == 0 ||
+            self.textFieldPasswordRepeat.text?.count == 0 {
+            self.displayMessage(title: "Error", message: "Please enter values in the required fields")
+            return false
+        } else if self.verifyPasswords() {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private func verifyPasswords() -> Bool {
+        if self.textFieldPassword.text != self.textFieldPasswordRepeat.text {
+            self.displayMessage(title: "Error", message: "Passwords don't match.")
+            return false
+        }
+        
+        return true
+    }
+    
+    private func displayMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+
+        self.present(alert, animated: true)
     }
     
     // MARK: UIResponders
     @objc private func buttonClose_TouchUpInside(sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+    @objc private func buttonApply_TouchUpInside(sender: UIButton) {
+        if self.changePassword() {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    @objc private func gestureTap_Tap(gesture: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
     
     // MARK: - Public API
     
-    
+    // MARK: Delegate Methods
+    public func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = self.colorInProgress.cgColor
+        textField.becomeFirstResponder()
+    }
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == "" {
+            textField.layer.borderColor = self.colorRequired.cgColor
+        } else {
+            textField.layer.borderColor = self.colorCompleted.cgColor
+        }
+        
+        textField.resignFirstResponder()
+    }
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.textFieldEmail {
+            self.textFieldName.becomeFirstResponder()
+        } else if textField == self.textFieldName {
+            self.textFieldPassword.becomeFirstResponder()
+        } else if textField == self.textFieldPassword {
+            self.textFieldPasswordRepeat.becomeFirstResponder()
+        }
+        textField.resignFirstResponder()
+        
+        return true
+    }
     
 }
